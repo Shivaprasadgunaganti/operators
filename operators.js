fe9b5a9fcb12a825e459a662fdca74d5aec393d2
shiva-pasad <Shivaprasadgunaganti@gmail.com>
2024-04-18 09:15:12 +0530

    dayone

---
+++ operators.js
@@ -0,0 +1,51 @@
+//ADDITITON
+var a =3,b=3;
+var sum=a+b;
+console.log('addition',sum);
+var  a =7
+    b=a++
+console.log('increment:',b)
+var a =7
+var    b=++a
+console.log('increment:',b) 
+
+//SUBSTRACTION
+  var a=7,b=2
+  var sub=a-b
+  console.log('substraction:',sub)
+    var a =7
+     var b=--a
+     console.log(b)
+
+//MULTIPLY
+  var a =5,b=3
+  var mul=a*b
+  console.log('multiply:',mul)
+
+  //DIVIDE
+  var a =10,b=3
+  var div=a/b
+  console.log('division:',div)
+
+// // PERCENTAGE
+     var a =7,b=9
+     var rem=a%b
+     console.log('reminder:',rem)
+
+    // var a =5
+    //  a%2===0;
+  
+    // console.log(a)
+
+    // var a =5
+    // var b = 4
+    // (a>b)?('hehe'):('ehuhhe')
+    // console.log(a)
+
+// console.log(`sum of the two number is`)
+
+// console.log(`sum of ${a} ${b}the two number is ${sum}`)
+
+// let rem=4%3;
+
+// console.log(`sum of ${a} ${b}the two number is ${rem}`)
